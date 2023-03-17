b14276d0fcf48d0f4bc8fd0e4d372a7f5a43d34b
symphony <jciurc@gmail.com>
2023-03-16 22:13:37 -0700

    fix enemy collision. set up ability timer

---
+++ scenes/ability/sword_ability_controller/sword_ability_controller.gd
@@ -0,0 +1,11 @@
+extends Node
+
+@export var sword_ability: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Timer.timeout.connect(on_timer_timeout)
+
+
+func on_timer_timeout():
+	pass
