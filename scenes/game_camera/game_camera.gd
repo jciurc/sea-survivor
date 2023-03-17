ce11c4cd1e23834d88834bc75e8d300c22654823
symphony <jciurc@gmail.com>
2023-03-16 21:48:58 -0700

    import basic enemy

--- scenes/game_camera/game_camera.gd
+++ scenes/game_camera/game_camera.gd
@@ -12,6 +12,7 @@ func _process(delta):
 	acquire_target()
 	global_position = global_position.lerp(target_position, 1.0 - exp(-delta * 10))
 	
+	
 func acquire_target():
 	var player_nodes = get_tree().get_nodes_in_group('player')
 	if player_nodes.size() > 0:
