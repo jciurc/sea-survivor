24ecc95a9a3cbb32ce023ce1f12adad01995df74
symphony <jciurc@gmail.com>
2023-03-16 22:52:25 -0700

    fix sword instantiation

--- scenes/ability/sword_ability_controller/sword_ability_controller.gd
+++ scenes/ability/sword_ability_controller/sword_ability_controller.gd
@@ -2,7 +2,6 @@ extends Node
 
 @export var sword_ability: PackedScene
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Timer.timeout.connect(on_timer_timeout)
@@ -16,4 +15,3 @@ func on_timer_timeout():
 	var sword_instance = sword_ability.instantiate() as Node2D
 	sword_instance.global_position = player.global_position
 	player.get_parent().add_child(sword_instance)
-	
