115143fab1a62d232fec3ded317fa1fbf8bc5cac
symphony <jciurc@gmail.com>
2023-03-17 21:31:42 -0700

    spawn sword on nearest enemy

--- scenes/ability/sword_ability_controller/sword_ability_controller.gd
+++ scenes/ability/sword_ability_controller/sword_ability_controller.gd
@@ -1,5 +1,7 @@
 extends Node
 
+const MAX_RANGE = 150
+
 @export var sword_ability: PackedScene
 
 # Called when the node enters the scene tree for the first time.
@@ -8,10 +10,24 @@ func _ready():
 
 
 func on_timer_timeout():
-	var player = get_tree().get_first_node_in_group("player") as Node2D
-	if !player:
-		return
+	
+	
+	var enemies = get_tree().get_nodes_in_group('enemy')
+	var player = get_tree().get_first_node_in_group('player') as Node2D
+	if !player: return
+	
+	enemies = enemies.filter(func(enemy: Node2D):
+		return enemy.global_position.distance_squared_to(player.global_position) < pow(MAX_RANGE, 2)
+	)
+	
+	if !enemies.size(): return
+	
+	enemies.sort_custom(func(a: Node2D, b: Node2D): 
+		var a_distance = a.global_position.distance_squared_to(player.global_position)
+		var b_distance = b.global_position.distance_squared_to(player.global_position)
+		return a_distance < b_distance
+	)
 	
 	var sword_instance = sword_ability.instantiate() as Node2D
-	sword_instance.global_position = player.global_position
+	sword_instance.global_position = enemies[0].global_position
 	player.get_parent().add_child(sword_instance)
