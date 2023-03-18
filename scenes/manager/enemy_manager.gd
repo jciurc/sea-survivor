4b26ee1c65acb97db3f590b810404197e5eba98d
symphony <jciurc@gmail.com>
2023-03-17 22:08:08 -0700

    add enemy spawner

---
+++ scenes/manager/enemy_manager.gd
@@ -0,0 +1,15 @@
+extends Node
+
+const SPAWN_RADIUS = 330
+
+@export var basic_enemy_scene: PackedScene
+
+func _ready():
+	$Timer.timeout.connect(on_timer_timeout)
+	
+func on_timer_timeout(): 
+	var player =get_tree().get_first_node_in_group('player') as Node2D
+	if !player: return
+	
+	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
+	var spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
