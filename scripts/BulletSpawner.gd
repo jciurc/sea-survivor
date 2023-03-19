e8e7cd00c9548a71ac1e1ad1122642f889a471a0
symphony <jciurc@gmail.com>
2023-03-18 19:02:48 -0700

    continue with bullet spawner scene

--- scripts/BulletSpawner.gd
+++ scripts/BulletSpawner.gd
@@ -1,15 +1,24 @@
 extends Node
 
 @export var bullet_scene: PackedScene
-var player: Node2D
+@export var initial_bullet_count: int
+
+const SPAWN_RADIUS = 20
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if !bullet_scene: return
-	$"../Timer".connect('timeout', spawn_bullet)
-	player = get_tree().get_first_node_in_group('player') as Node2D
+	$"../FirerateTimer".connect('timeout', spawn_bullet)
+
 
 func spawn_bullet():
+	var player = get_tree().get_first_node_in_group('player') as Node2D
+	if !player: return
+	
 	var bullet_instance = bullet_scene.instantiate() as Node2D
 	bullet_instance.global_position = player.global_position
 	player.get_parent().add_child(bullet_instance)
+
+	
+	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
+	var spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
