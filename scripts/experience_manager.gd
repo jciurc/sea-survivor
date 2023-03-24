7dc49c1700813703d04c64691da3bb2d9ee45d2f
symphony <jciurc@gmail.com>
2023-03-23 17:57:05 -0700

    adjust xp bar. update experience signal

--- scripts/experience_manager.gd
+++ scripts/experience_manager.gd
@@ -9,3 +9,8 @@ func _ready():
 
 func increment_experience(number: float):
 	current_experience += number
+	print(current_experience)
+
+
+func on_experience_vial_collected(number: float):
+	increment_experience(number)
