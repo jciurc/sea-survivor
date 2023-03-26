f0992809ec75d3ec8a3560cbef4babb3fb297023
symphony <jciurc@gmail.com>
2023-03-25 20:53:07 -0700

    fix upgrade card scene

--- scripts/upgrade_manager.gd
+++ scripts/upgrade_manager.gd
@@ -16,7 +16,8 @@ func on_level_up(current_level: int):
 
 	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
 	add_child(upgrade_screen_instance)
-	upgrade_screen_instance.set_ability_upgrades([chosen_upgrade])
+	upgrade_screen_instance.set_ability_upgrades([chosen_upgrade] as Array[AbilityUpgrade])
+
 
 func apply_upgrade(upgrade: AbilityUpgrade):
 	var has_upgrade = upgrade.has(upgrade.id)
