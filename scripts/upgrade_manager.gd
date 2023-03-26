ac9cbfbe9496eabe46219f0a059d36bd45bb265e
symphony <jciurc@gmail.com>
2023-03-25 20:50:29 -0700

    setup upgrade manager to instantiate upgrade card scene

--- scripts/upgrade_manager.gd
+++ scripts/upgrade_manager.gd
@@ -2,6 +2,7 @@ extends Node
 
 @export var upgrade_pool: Array[AbilityUpgrade]
 @export var experience_manager: ExperienceManager
+@export var upgrade_screen_scene: PackedScene
 
 var current_upgrades = {}
 
@@ -13,11 +14,16 @@ func on_level_up(current_level: int):
 	var chosen_upgrade = upgrade_pool.pick_random() as AbilityUpgrade
 	if !chosen_upgrade: return
 
-	var has_upgrade = current_upgrades.has(chosen_upgrade.id)
+	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
+	add_child(upgrade_screen_instance)
+	upgrade_screen_instance.set_ability_upgrades([chosen_upgrade])
+
+func apply_upgrade(upgrade: AbilityUpgrade):
+	var has_upgrade = upgrade.has(upgrade.id)
 	if !has_upgrade:
-		current_upgrades[chosen_upgrade.id] = {
-			'resource': chosen_upgrade,
+		upgrade[upgrade.id] = {
+			'resource': upgrade,
 			'quantity': 1
 		}
 	else :
-		current_upgrades[chosen_upgrade.id]["quantity"] += 1
+		upgrade[upgrade.id]["quantity"] += 1
