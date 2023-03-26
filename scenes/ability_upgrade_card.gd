d43c7f6f97dff9c62430ed491e26835464a1072e
symphony <jciurc@gmail.com>
2023-03-25 20:43:06 -0700

    set up ability card ui script

--- scenes/ability_upgrade_card.gd
+++ scenes/ability_upgrade_card.gd
@@ -1,1 +1,9 @@
 extends PanelContainer
+
+@onready var name_label = %NameLabel
+@onready var description_label = %DescriptionLabel
+
+
+func set_ability_upgrade(upgrade: AbilityUpgrade):
+	name_label.text = upgrade.name
+	description_label.text = upgrade.description
