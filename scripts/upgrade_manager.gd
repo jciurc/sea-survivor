909324558b531fa11b76feee9f2a4413cbd17676
symphony <jciurc@gmail.com>
2023-03-26 19:02:25 -0700

    listen for upgrade selected signal in upgrade manager

--- scripts/upgrade_manager.gd
+++ scripts/upgrade_manager.gd
@@ -17,6 +17,7 @@ func on_level_up(current_level: int):
 	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
 	add_child(upgrade_screen_instance)
 	upgrade_screen_instance.set_ability_upgrades([chosen_upgrade] as Array[AbilityUpgrade])
+	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
 
 
 func apply_upgrade(upgrade: AbilityUpgrade):
@@ -28,3 +29,7 @@ func apply_upgrade(upgrade: AbilityUpgrade):
 		}
 	else :
 		upgrade[upgrade.id]["quantity"] += 1
+
+
+func on_upgrade_selected(upgrade: AbilityUpgrade):
+	apply_upgrade(upgrade)
