a8f0bbb872144c63ff72aafeb079b73226f605d5
symphony <jciurc@gmail.com>
2023-03-26 18:20:38 -0700

    create on gui input signal

--- scripts/ability_upgrade_card.gd
+++ scripts/ability_upgrade_card.gd
@@ -5,6 +5,14 @@ class_name UIAbilityUpgradeCard
 @onready var description_label = %DescriptionLabel
 
 
+func _ready():
+	gui_input.connect(on_gui_input)
+
 func set_ability_upgrade(upgrade: AbilityUpgrade):
 	name_label.text = upgrade.name
 	description_label.text = upgrade.description
+
+
+func on_gui_input(event: InputEvent):
+	pass
+
