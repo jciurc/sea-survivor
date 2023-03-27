64cb4a297e857381e603db522d3f1b731002e851
symphony <jciurc@gmail.com>
2023-03-26 18:51:33 -0700

    set up left click signal

--- scripts/ability_upgrade_card.gd
+++ scripts/ability_upgrade_card.gd
@@ -1,6 +1,8 @@
 extends PanelContainer
 class_name UIAbilityUpgradeCard
 
+signal selected
+
 @onready var name_label = %NameLabel
 @onready var description_label = %DescriptionLabel
 
@@ -8,11 +10,12 @@ class_name UIAbilityUpgradeCard
 func _ready():
 	gui_input.connect(on_gui_input)
 
+
 func set_ability_upgrade(upgrade: AbilityUpgrade):
 	name_label.text = upgrade.name
 	description_label.text = upgrade.description
 
 
 func on_gui_input(event: InputEvent):
-	pass
-
+	if event.is_action_pressed("left_click"):
+		selected.emit()
