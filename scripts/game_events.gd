ea15cf8e783e9201d19e4ba308b0569004e8d155
symphony <jciurc@gmail.com>
2023-03-27 19:11:44 -0700

    add ability upgrade added signal

--- scripts/game_events.gd
+++ scripts/game_events.gd
@@ -1,8 +1,12 @@
 extends Node
 
 signal experience_vial_collected(number: float)
+signal ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary)
 
 
 func emit_collect(number: float):
 	experience_vial_collected.emit(number)
 
+
+func emit_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary):
+	ability_upgrade_added.emit(upgrade, current_upgrades)
