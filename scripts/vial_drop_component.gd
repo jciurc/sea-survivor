ceba90260119afce69b19182e2f2bbf854045524
symphony <jciurc@gmail.com>
2023-03-28 19:21:06 -0700

    create entities and foreground layer

--- scripts/vial_drop_component.gd
+++ scripts/vial_drop_component.gd
@@ -16,5 +16,6 @@ func on_died():
 
 	var spawn_position = owner.global_position
 	var vial_instance: Node2D = vial_scene.instantiate()
-	owner.get_parent().add_child(vial_instance)
+	var entities_layer = get_tree().get_first_node_in_group("entities_layer") as Node2D
+	entities_layer.add_child(vial_instance)
 	vial_instance.global_position = spawn_position
