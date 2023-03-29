f0eed8162ac63ee173cf929dec3987685134fdab
symphony <jciurc@gmail.com>
2023-03-28 19:32:25 -0700

    create body entered and exited signals on player

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,12 +3,13 @@ extends CharacterBody2D
 const MAX_SPEED = 150
 const ACCELERATION_SMOOTHING = 40
 
-# Called when the node enters the scene tree for the first time.
+var bodies_currently_colliding = 0;
+
 func _ready():
-	pass # Replace with function body.
+	$CollisionArea2D.body_entered.connect(on_body_entered)
+	$CollisionArea2D.body_exited.connect(on_body_exited)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var movement_vector = get_movement_vector()
 	var direction = movement_vector.normalized()
@@ -23,3 +24,10 @@ func get_movement_vector():
 	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	return Vector2(x_movement, y_movement)
 
+
+func on_body_entered(other_body: Node2D):
+	bodies_currently_colliding += 1
+
+
+func on_body_exited(other_body: Node2D):
+	bodies_currently_colliding -= 1
