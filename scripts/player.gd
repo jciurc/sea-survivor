1d0a235e718729df58e4856edfb093d1ddfa9f34
symphony <jciurc@gmail.com>
2023-03-28 19:28:12 -0700

    add collision area2d on player

--- scripts/player.gd
+++ scripts/player.gd
@@ -22,3 +22,4 @@ func get_movement_vector():
 	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	return Vector2(x_movement, y_movement)
+
