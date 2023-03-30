c8caa0f06e9ff35f87ddedf2996278d4c80b0dae
symphony <jciurc@gmail.com>
2023-03-29 18:57:52 -0700

    fix victory screen buttons

--- scripts/victory_scene.gd
+++ scripts/victory_scene.gd
@@ -3,8 +3,8 @@ extends CanvasLayer
 
 func _ready():
 	get_tree().paused = true
-	$RestartButton.pressed.connect(on_restart_pressed)
-	$QuitButton.pressed.connect(on_quit_pressed)
+	%RestartButton.pressed.connect(on_restart_pressed)
+	%QuitButton.pressed.connect(on_quit_pressed)
 
 
 func on_restart_pressed():
