dcf8f4e83844eb63800549bd6a6f2749d31ca26a
symphony <jciurc@gmail.com>
2023-04-02 18:51:46 -0700

    set up axe rotation

--- scripts/axe_ability.gd
+++ scripts/axe_ability.gd
@@ -1,5 +1,20 @@
 extends Node2D
 
+const MAX_RADIUS = 100
+
 
 func _ready():
 	var tween = create_tween()
+	tween.tween_method(tween_method, 0.0, 2.0, 2)
+
+
+func tween_method(rotations: float):
+	var current_radius = (rotations / 2) * MAX_RADIUS
+	var current_direction = Vector2.RIGHT.rotated(rotations * TAU)
+
+	var root_position = Vector2.ZERO
+	var player = get_tree().get_first_node_in_group("player")
+	if !player: return
+
+	root_position = player.global_position
+	global_position = root_position + (current_direction * current_radius)
