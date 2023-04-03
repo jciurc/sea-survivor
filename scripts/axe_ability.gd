1fd9caed6ac25e77e225cf9893e8311dedbc64d3
symphony <jciurc@gmail.com>
2023-04-02 18:53:42 -0700

    remove extra root var in axe animation

--- scripts/axe_ability.gd
+++ scripts/axe_ability.gd
@@ -12,9 +12,7 @@ func tween_method(rotations: float):
 	var current_radius = (rotations / 2) * MAX_RADIUS
 	var current_direction = Vector2.RIGHT.rotated(rotations * TAU)
 
-	var root_position = Vector2.ZERO
 	var player = get_tree().get_first_node_in_group("player")
 	if !player: return
 
-	root_position = player.global_position
-	global_position = root_position + (current_direction * current_radius)
+	global_position = player.global_position + (current_direction * current_radius)
