1787aefb93ecc92c5553d464e4407b88fd9e9694
symphony <jciurc@gmail.com>
2023-04-03 19:00:26 -0700

    add random starting rotation to axe ability

--- scripts/axe_ability.gd
+++ scripts/axe_ability.gd
@@ -4,8 +4,11 @@ const MAX_RADIUS = 100
 
 @onready var hitbox_component = $HitboxComponent
 
+var base_rotation = Vector2.RIGHT
+
 
 func _ready():
+	base_rotation = Vector2.RIGHT.rotated(randf_range(0, TAU))
 	var tween = create_tween()
 	tween.tween_method(tween_method, 0.0, 2.0, 3)
 	tween.tween_callback(queue_free)
@@ -13,7 +16,7 @@ func _ready():
 
 func tween_method(rotations: float):
 	var current_radius = (rotations / 2) * MAX_RADIUS
-	var current_direction = Vector2.RIGHT.rotated(rotations * TAU)
+	var current_direction = base_rotation.rotated(rotations * TAU)
 
 	var player = get_tree().get_first_node_in_group("player")
 	if !player: return
