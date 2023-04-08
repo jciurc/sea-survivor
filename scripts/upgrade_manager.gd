82a8caefbbd78f21d8e6173736973935e5edc91e
symphony <jciurc@gmail.com>
2023-04-08 14:31:34 -0700

    update check for empty upgrade pool

--- scripts/upgrade_manager.gd
+++ scripts/upgrade_manager.gd
@@ -34,10 +34,10 @@ func pick_upgrades():
 	var chosen_upgrades: Array[AbilityUpgrade] = []
 	var filtered_upgrades = upgrade_pool.duplicate()
 	for i in number_of_upgrade_choices:
-		var chosen_upgrade = filtered_upgrades.pick_random() as AbilityUpgrade
-		if !chosen_upgrade:
+		if filtered_upgrades.size() <= 0:
 			break
 
+		var chosen_upgrade = filtered_upgrades.pick_random() as AbilityUpgrade
 		chosen_upgrades.append(chosen_upgrade)
 		filtered_upgrades = filtered_upgrades.filter(func (upgrade): return upgrade.id != chosen_upgrade.id)
 
