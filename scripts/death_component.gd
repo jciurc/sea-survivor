6242191b56ad4c9c472a61da22f8a4aa14499d7b
symphony <jciurc@gmail.com>
2023-04-10 16:20:02 -0700

    attach death component to basic enemy

--- scripts/death_component.gd
+++ scripts/death_component.gd
@@ -10,7 +10,12 @@ func _ready():
 
 
 func on_died():
+	if !owner || not owner is Node2D:
+		return
+
+	var spawn_position = owner.global_position
 	var entities = get_tree().get_first_node_in_group("entities_layer")
 	get_parent().remove_child(self)
 	entities.add_child(self)
+	global_position = spawn_position
 	$AnimationPlayer.play("default")
