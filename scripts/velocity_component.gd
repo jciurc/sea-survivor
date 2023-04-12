77ffdc070ef9f15db84b67f8c10839b38f10ac22
symphony <jciurc@gmail.com>
2023-04-11 17:18:57 -0700

    build accelerate to player helper function

--- scripts/velocity_component.gd
+++ scripts/velocity_component.gd
@@ -6,8 +6,10 @@ extends Node
 var velocity = Vector2.ZERO
 
 
-func accelerate_to_player():
-	pass
+func move(character_body: CharacterBody2D):
+	character_body.velocity = velocity
+	character_body.move_and_slide()
+	velocity = character_body.velocity
 
 
 func accelerate_in_direction(direction: Vector2):
@@ -15,7 +17,12 @@ func accelerate_in_direction(direction: Vector2):
 	velocity = velocity.lerp(desired_velocity, 1 - exp(-acceleration * get_process_delta_time()))
 
 
-func move(character_body: CharacterBody2D):
-	character_body.velocity = velocity
-	character_body.move_and_slide()
-	velocity = character_body.velocity
+func accelerate_to_player():
+	var owner_node2d = owner as Node2D
+	if !owner_node2d: return
+
+	var player = get_tree().get_first_node_in_group("player")
+	if !player: return
+
+	var direction = (player.global_position - owner_node2d.global_position).normalize()
+	accelerate_in_direction(direction)
