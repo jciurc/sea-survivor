92d53e140783e7582fac1c72689d90650408f63a
symphony <jciurc@gmail.com>
2023-04-11 17:15:31 -0700

    set up move function in velocity component

--- scripts/velocity_component.gd
+++ scripts/velocity_component.gd
@@ -12,8 +12,10 @@ func accelerate_to_player():
 
 func accelerate_in_direction(direction: Vector2):
 	var desired_velocity = direction * max_speed
-	velocity = velocity.lerp(desired_velocity, 1 - exp(-acceleration * get_process_delta_time())
+	velocity = velocity.lerp(desired_velocity, 1 - exp(-acceleration * get_process_delta_time()))
 
 
 func move(character_body: CharacterBody2D):
-	pass
+	character_body.velocity = velocity
+	character_body.move_and_slide()
+	velocity = character_body.velocity
