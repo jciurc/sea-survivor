b10d78931836fd45d1c599863f440ddcf514fcde
symphony <jciurc@gmail.com>
2023-04-12 18:23:30 -0700

    time wizard movement to animation. fix animation speed

--- scripts/velocity_component.gd
+++ scripts/velocity_component.gd
@@ -17,6 +17,10 @@ func accelerate_in_direction(direction: Vector2):
 	velocity = velocity.lerp(desired_velocity, 1 - exp(-acceleration * get_process_delta_time()))
 
 
+func decelerate():
+	accelerate_in_direction(Vector2.ZERO)
+
+
 func accelerate_to_player():
 	var owner_node2d = owner as Node2D
 	if !owner_node2d: return
--- scripts/wizard_enemy.gd
+++ scripts/wizard_enemy.gd
@@ -3,11 +3,22 @@ extends CharacterBody2D
 @onready var velocity_component = $VelocityComponent
 @onready var visuals = $Visuals
 
+var is_moving = false
+
 
 func _process(delta):
-	velocity_component.accelerate_to_player()
+	if is_moving:
+		velocity_component.accelerate_to_player()
+	else:
+		velocity_component.decelerate()
+
 	velocity_component.move(self)
 
 	var move_sign = sign(velocity_component.velocity.x)
 	if move_sign != 0:
 		visuals.scale = Vector2(move_sign, 1)
+
+
+
+func set_is_moving(moving: bool):
+	is_moving = moving
