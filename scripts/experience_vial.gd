3cfe8143a7179d3cf4b5076e519248095b9af0ed
symphony <jciurc@gmail.com>
2023-04-13 17:18:33 -0700

    set up tween on vial component

--- scripts/experience_vial.gd
+++ scripts/experience_vial.gd
@@ -1,11 +1,24 @@
 extends Node2D
 
+@onready var animation_player = $AnimationPlayer
+
 
 func _ready():
 	$Area2D.area_entered.connect(on_area_entered)
 
 
+func tween_collect(percent: float, start_position: Vector2):
+	var player = get_tree().get_first_node_in_group("player")
+	if !player: return
+
+	global_position = start_position.lerp(player.global_position, percent)
+
+
 func on_area_entered(other_area: Area2D):
+	var tween = create_tween()
+	tween.tween_method(tween_collect.bind(global_position), 0.0, 1.0, 1)
+
+
 	GameEvents.emit_collect(1)
 	queue_free()
 
