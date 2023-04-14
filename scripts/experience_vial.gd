5954338ff9f1be7ff80b595e6df37be6d95aa48e
symphony <jciurc@gmail.com>
2023-04-13 17:44:41 -0700

    add lerp to vial rotation

--- scripts/experience_vial.gd
+++ scripts/experience_vial.gd
@@ -13,7 +13,8 @@ func tween_collect(percent: float, start_position: Vector2):
 
 	global_position = start_position.lerp(player.global_position, percent)
 	var direction_from_start = player.global_position - start_position
-	rotation_degrees = rad_to_deg(direction_from_start.angle()) + 90
+	var target_rotation =  direction_from_start.angle() + deg_to_rad(90)
+	rotation = lerp_angle(rotation, target_rotation, 1 - exp(-2 * get_process_delta_time()))
 
 
 func collect():
