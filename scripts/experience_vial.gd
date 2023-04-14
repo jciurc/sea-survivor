8e2d99d024d1b6ea3e4bae2deb957a47c0092b38
symphony <jciurc@gmail.com>
2023-04-13 17:22:45 -0700

    add ease to vial tween

--- scripts/experience_vial.gd
+++ scripts/experience_vial.gd
@@ -21,5 +21,8 @@ func collect():
 
 func on_area_entered(other_area: Area2D):
 	var tween = create_tween()
-	tween.tween_method(tween_collect.bind(global_position), 0.0, 1.0, 1)
+	tween.tween_method(tween_collect.bind(global_position), 0.0, 1.0, 0.5)\
+		.set_ease(Tween.EASE_IN)\
+		.set_trans(Tween.TRANS_BACK)
+
 	tween.tween_callback(collect)
