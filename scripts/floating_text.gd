0b7ae4d4ae07ac31734e108404e7fe37678f0a22
symphony <jciurc@gmail.com>
2023-04-17 18:05:13 -0700

    add scale to floating text tween

--- scripts/floating_text.gd
+++ scripts/floating_text.gd
@@ -9,10 +9,20 @@ func start(text: String):
 	$Label.text = text
 
 	var tween = create_tween()
+	tween.set_parallel()
 	tween.tween_property(self, "global_position", global_position + (Vector2.UP * 16), .3)\
 		.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 
-	tween.tween_property(self, "global_position", global_position + (Vector2.UP * 48), .4)\
+	tween.tween_property(self, "scale", Vector2.ONE, .3)\
+		.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
+
+	tween.chain()
+
+	tween.tween_property(self, "global_position", global_position + (Vector2.UP * 48), .3)\
 		.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
 
+	tween.tween_property(self, "scale", Vector2.ZERO, .3)\
+		.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
+
+	tween.chain()
 	tween.tween_callback(queue_free)
