b177103849e25782a6bc5e9b80fe4e72dc6a57f3
symphony <jciurc@gmail.com>
2023-04-17 17:48:01 -0700

    add tween to floating text

---
+++ scripts/Label.gd
@@ -0,0 +1,16 @@
+extends Label
+
+
+func _ready():
+	pass
+
+
+func start(text: String):
+	$Label.text = text
+
+	var tween = create_tween()
+	tween.tween_property(self, "global_position", global_position + (Vector2.UP * 16), .3)\
+		.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+
+	tween.tween_property(self, "global_position", global_position + (Vector2.UP * 48), .4)\
+		.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
