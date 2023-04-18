d398fe5e1f70b06b2d877b2c63b4feddca469125
symphony <jciurc@gmail.com>
2023-04-17 18:17:07 -0700

    add scale bounce to floating text

--- scripts/floating_text.gd
+++ scripts/floating_text.gd
@@ -13,16 +13,17 @@ func start(text: String):
 	tween.tween_property(self, "global_position", global_position + (Vector2.UP * 16), .3)\
 		.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 
-	tween.tween_property(self, "scale", Vector2.ONE, .3)\
-		.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
-
 	tween.chain()
-
-	tween.tween_property(self, "global_position", global_position + (Vector2.UP * 48), .3)\
+	tween.tween_property(self, "global_position", global_position + (Vector2.UP * 48), .5)\
 		.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
-
-	tween.tween_property(self, "scale", Vector2.ZERO, .3)\
+	tween.tween_property(self, "scale", Vector2.ZERO, .5)\
 		.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
 
 	tween.chain()
 	tween.tween_callback(queue_free)
+
+	var bounce_tween = create_tween()
+	bounce_tween.tween_property(self, "scale", Vector2.ONE * 1.5, .15)\
+		.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+	bounce_tween.tween_property(self, "scale", Vector2.ONE, .15)\
+		.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
