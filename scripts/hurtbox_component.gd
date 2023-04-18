730af3f8eee16d11ee13e971c1f05e98944e341f
symphony <jciurc@gmail.com>
2023-04-17 17:51:23 -0700

    instantiate floating text in hurtbox component

--- scripts/hurtbox_component.gd
+++ scripts/hurtbox_component.gd
@@ -3,6 +3,8 @@ class_name HurtboxComponent
 
 @export var health_component: HealthComponent
 
+var floating_text_scene = preload("res://scenes/floating_text.tscn")
+
 
 func _ready():
 	area_entered.connect(on_area_entered)
@@ -13,3 +15,8 @@ func on_area_entered(hitbox_component: HitboxComponent):
 	if !health_component: return
 
 	health_component.damage(hitbox_component.damage)
+
+	var floating_text = floating_text_scene.instantiate() as Node2D
+	get_tree().get_first_node_in_group("foreground_layer").add_child(floating_text)
+
+	floating_text.position = global_position
