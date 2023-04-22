e5ac3e72dfc52348669c782a4d0abdc000ea0c57
symphony <jciurc@gmail.com>
2023-04-21 19:06:13 -0700

    fix weighted table pick items function

--- scripts/weighted_table.gd
+++ scripts/weighted_table.gd
@@ -12,21 +12,22 @@ func pick_item(exclude_items: Array = []):
 	var adjusted_items: Array[Dictionary] = []
 	var adjusted_weight_sum = weight_sum
 
+
 	if exclude_items.size() > 0:
 		adjusted_items = []
 		adjusted_weight_sum = 0
+
 		for item in items:
 			if item.item in exclude_items:
 				continue
 			adjusted_items.append(item)
 			adjusted_weight_sum += item.weight
 
-	var chosen_weight = randi_range(1, adjusted_weight_sum)
+	var chosen_weight = randi_range(1, weight_sum)
 	var iteration_sum = 0
-	for item in adjusted_items:
+	for item in items:
 		iteration_sum += item.weight
 		if chosen_weight <= iteration_sum:
-			print(item)
 			return item.item
 
 
