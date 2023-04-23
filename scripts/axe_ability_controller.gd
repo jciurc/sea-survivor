6f1462901cce976caf4c412324b7fcf6a2e05a6f
symphony <jciurc@gmail.com>
2023-04-22 17:21:47 -0700

    add damage scaling to axe upgrade

--- scripts/axe_ability_controller.gd
+++ scripts/axe_ability_controller.gd
@@ -3,10 +3,11 @@ extends Node
 @export var axe_ability_scene: PackedScene
 
 var damage = 10
-
+var damage_percent = 1.0
 
 func _ready():
 	$Timer.timeout.connect(on_timer_timeout)
+	GameEvents.ability_upgrade_added.connect(on_ability_upgrade_added)
 
 
 func on_timer_timeout():
@@ -20,3 +21,8 @@ func on_timer_timeout():
 	foreground.add_child(axe_instance)
 	axe_instance.global_position = player.global_position
 	axe_instance.hitbox_component.damage = damage
+
+
+func on_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary):
+	if upgrade.id == "axe_damage":
+		damage_percent = 1 + (current_upgrades.axe_damage.quantity * .15)
