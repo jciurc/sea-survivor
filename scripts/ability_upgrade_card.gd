035d6f58b27bf6bc37ed6cb30d8f9c1ef70faf09
symphony <jciurc@gmail.com>
2023-04-29 14:24:11 -0700

    play selected animation on card select

--- scripts/ability_upgrade_card.gd
+++ scripts/ability_upgrade_card.gd
@@ -6,6 +6,8 @@ signal selected
 @onready var name_label = %NameLabel
 @onready var description_label = %DescriptionLabel
 
+var disabled = false
+
 
 func _ready():
 	gui_input.connect(on_gui_input)
@@ -24,9 +26,13 @@ func set_ability_upgrade(upgrade: AbilityUpgrade):
 
 
 func on_gui_input(event: InputEvent):
+	if disabled: return
 	if event.is_action_pressed("left_click"):
+		disabled = true
+		await $AnimationPlayer.play("selected")
 		selected.emit()
 
 
 func on_mouse_entered():
+	if disabled: return
 	$HoverAnimationPlayer.play("hover")
