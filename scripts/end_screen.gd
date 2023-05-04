86478b5be1df7617bd5d5dde786e3877f987fe3f
symphony <jciurc@gmail.com>
2023-05-04 09:24:59 -0700

    ease in end screen card

--- scripts/end_screen.gd
+++ scripts/end_screen.gd
@@ -7,7 +7,8 @@ func _ready():
 	panel_container.pivot_offset = panel_container.size / 2
 	var tween = create_tween()
 	tween.tween_property(panel_container, "scale", Vector2.ZERO, 0)
-	tween.tween_property(panel_container, "scale", Vector2.ONE, .3)
+	tween.tween_property(panel_container, "scale", Vector2.ONE, .3)\
+		.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
 
 	get_tree().paused = true
 	%RestartButton.pressed.connect(on_restart_pressed)
