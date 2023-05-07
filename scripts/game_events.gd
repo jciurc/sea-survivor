9ad33c98256f524806d6a55031e47b23d22483a5
symphony <jciurc@gmail.com>
2023-05-07 14:51:46 -0700

    add hit flash animation to vignette

--- scripts/game_events.gd
+++ scripts/game_events.gd
@@ -2,7 +2,7 @@ extends Node
 
 signal experience_vial_collected(number: float)
 signal ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary)
-
+signal player_damaged
 
 func emit_collect(number: float):
 	experience_vial_collected.emit(number)
@@ -10,3 +10,7 @@ func emit_collect(number: float):
 
 func emit_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary):
 	ability_upgrade_added.emit(upgrade, current_upgrades)
+
+
+func emit_player_damaged():
+	player_damaged.emit()
--- scripts/player.gd
+++ scripts/player.gd
@@ -60,6 +60,7 @@ func on_damage_interval_timer_timeout():
 
 
 func on_health_changed():
+	GameEvents.emit_player_damaged()
 	update_health_display()
 
 
