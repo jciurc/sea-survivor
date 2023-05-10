63d5b8d9fc1012f72cdebca58e9fcef10c3a21bb
symphony <jciurc@gmail.com>
2023-05-10 16:15:51 -0700

    add random hit sound on wizard enemy hit

--- scripts/wizard_enemy.gd
+++ scripts/wizard_enemy.gd
@@ -6,6 +6,10 @@ extends CharacterBody2D
 var is_moving = false
 
 
+func _ready():
+	$HurtboxComponent.hit.connect(on_hit)
+
+
 func _process(delta):
 	if is_moving:
 		velocity_component.accelerate_to_player()
@@ -22,3 +26,7 @@ func _process(delta):
 
 func set_is_moving(moving: bool):
 	is_moving = moving
+
+
+func on_hit():
+	$RandomHitAudio.play_random()
