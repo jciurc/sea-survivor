5b5fbbe70604ced10d37cfb23ceec3d17f322367
symphony <jciurc@gmail.com>
2023-05-13 15:28:22 -0700

    create random stream player component

---
+++ scripts/random_stream_player_component.gd
@@ -0,0 +1,17 @@
+extends AudioStreamPlayer
+
+@export var streams: Array[AudioStream]
+@export var randomize_pitch  = true
+@export var min_pitch = .9
+@export var max_pitch = 1.1
+
+func play_random():
+	if streams == null || streams.size() == 0:
+		return
+
+	pitch_scale = 1
+	if randomize_pitch:
+		pitch_scale = randf_range(min_pitch, max_pitch)
+
+	stream = streams.pick_random()
+	play()
