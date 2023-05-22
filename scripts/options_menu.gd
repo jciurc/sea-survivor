be306d2a79225c332327103ce0ce9d6128643821
symphony <jciurc@gmail.com>
2023-05-22 12:31:47 -0700

    set up window mode button functionality

--- scripts/options_menu.gd
+++ scripts/options_menu.gd
@@ -1,1 +1,13 @@
 extends CanvasLayer
+
+
+func _ready():
+	%WindowButton.pressed.connect(on_window_button_pressed)
+
+
+func on_window_button_pressed():
+	var mode = DisplayServer.window_get_mode()
+	if mode != DisplayServer.WINDOW_MODE_FULLSCREEN:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
