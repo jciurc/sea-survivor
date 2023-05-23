ce8c19e1bff3a51a03f52b5b2794e24ffd3106ab
symphony <jciurc@gmail.com>
2023-05-23 11:30:59 -0700

    update sliders to use audio bus values

--- scripts/options_menu.gd
+++ scripts/options_menu.gd
@@ -1,6 +1,8 @@
 extends CanvasLayer
 
 @onready var window_button: Button = %WindowButton
+@onready var music_slider = %MusicSlider
+@onready var sfx_slider = %SfxSlider
 
 
 func _ready():
@@ -11,6 +13,15 @@ func update_display():
 	window_button.text = "Windowed"
 	if DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_FULLSCREEN:
 		window_button.text = "Fullscreen"
+
+	sfx_slider.value = get_bus_volume_percent("sfx")
+	music_slider.value = get_bus_volume_percent("music")
+
+
+func get_bus_volume_percent(bus_name: String):
+	var bus_index = AudioServer.get_bus_index(bus_name)
+	var volume_db = AudioServer.get_bus_volume_db(bus_index)
+	return db_to_linear(volume_db)
 
 
 func on_window_button_pressed():
