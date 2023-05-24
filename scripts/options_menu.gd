3f7d32e86bcc80251ecc3aede1c01795891bbb42
symphony <jciurc@gmail.com>
2023-05-24 15:45:37 -0700

    link up sfx and music sliders

--- scripts/options_menu.gd
+++ scripts/options_menu.gd
@@ -7,7 +7,8 @@ extends CanvasLayer
 
 func _ready():
 	window_button.pressed.connect(on_window_button_pressed)
-	sfx_slider.changed.connect(on_slider_change.bind(sfx_slider, "sfx"))
+	sfx_slider.value_changed.connect(on_slider_change.bind("sfx"))
+	music_slider.value_changed.connect(on_slider_change.bind("music"))
 	update_display()
 
 
@@ -22,14 +23,12 @@ func update_display():
 
 func get_bus_volume_percent(bus_name: String):
 	var bus_index = AudioServer.get_bus_index(bus_name)
-	var volume_db = AudioServer.get_bus_volume_db(bus_index)
-	return db_to_linear(volume_db)
+	return db_to_linear(AudioServer.get_bus_volume_db(bus_index))
 
 
 func set_bus_volume_percent(bus_name: String, percent: float):
 	var bus_index = AudioServer.get_bus_index(bus_name)
-	var volume_db = linear_to_db(percent)
-	AudioServer.set_bus_volume_db(bus_index, volume_db)
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(percent))
 
 
 func on_window_button_pressed():
@@ -42,5 +41,5 @@ func on_window_button_pressed():
 	update_display()
 
 
-func on_slider_change(slider: HSlider, bus_name: String):
-	pass
+func on_slider_change(value: float, bus_name: String):
+	set_bus_volume_percent(bus_name, value)
