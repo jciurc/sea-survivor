bccda841719c644298f166fab269013d9bd0f39d
symphony <jciurc@gmail.com>
2023-05-29 13:23:20 -0700

    set up pause button

--- scripts/_main.gd
+++ scripts/_main.gd
@@ -2,9 +2,17 @@ extends Node
 
 @export var end_screen_scene: PackedScene
 
+var pause_menu_scene = preload("res://scenes/pause_menu.tscn")
+
 
 func _ready():
 	%Player.health_component.died.connect(on_player_died)
+
+
+func _unhandled_input(event):
+	if event.is_action_pressed("pause"):
+		add_child(pause_menu_scene.instantiate())
+		get_tree().root.set_input_as_handled()
 
 
 func on_player_died():
--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -2,6 +2,7 @@ extends CanvasLayer
 
 @onready var panel_container = %PanelContainer
 
+var is_closing = false
 
 func _ready():
 	get_tree().paused = true
@@ -14,7 +15,17 @@ func _ready():
 		.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
 
 
-func on_resume_pressed():
+func _unhandled_input(event):
+	if event.is_action_pressed("pause"):
+		close()
+		get_tree().root.set_input_as_handled()
+
+
+func close():
+	if is_closing:
+		return
+
+	is_closing = true
 	$AnimationPlayer.play_backwards("default")
 
 	var tween = create_tween()
@@ -25,3 +36,8 @@ func on_resume_pressed():
 	await tween.finished
 	get_tree().paused = false
 	queue_free()
+
+
+
+func on_resume_pressed():
+	close()
