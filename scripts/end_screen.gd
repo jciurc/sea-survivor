0cd5971ad877690e942c84160817b2146de4da6f
symphony <jciurc@gmail.com>
2023-05-31 14:09:51 -0700

    add transition to restart button on end screen

--- scripts/end_screen.gd
+++ scripts/end_screen.gd
@@ -30,6 +30,8 @@ func play_jingle(defeat: bool = false):
 
 
 func on_restart_pressed():
+	ScreenTransition.transition()
+	await ScreenTransition.transitioned_halfway
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/_main_game.tscn")
 
