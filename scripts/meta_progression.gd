0b37041a3d43903ae123df96dbba1dae7b5e12f0
symphony <jciurc@gmail.com>
2023-06-04 14:53:32 -0700

    write save file to disk. save on experience collected

--- scripts/meta_progression.gd
+++ scripts/meta_progression.gd
@@ -1,5 +1,7 @@
 extends Node
 
+const SAVE_FILE_PATH = "user://game.save"
+
 var save_data: Dictionary = {
 	"meta_upgrade_currency": 0,
 	"meta_upgrades": {},
@@ -8,6 +10,20 @@ var save_data: Dictionary = {
 
 func _ready():
 	GameEvents.experience_vial_collected.connect(on_experience_collected)
+	load_save_file()
+
+
+func load_save_file():
+	if not FileAccess.file_exists(SAVE_FILE_PATH):
+		return
+
+	var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.READ)
+	save_data = file.get_var()
+
+
+func save():
+	var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.WRITE)
+	file.store_var(save_data)
 
 
 func add_meta_upgrade(upgrade: MetaUpgrade):
@@ -19,3 +35,4 @@ func add_meta_upgrade(upgrade: MetaUpgrade):
 
 func on_experience_collected(amount: float):
 	save_data.meta_upgrade_currency += amount
+	save()
