2abd56ac9601603c7591114337982396b59a77cf
symphony <jciurc@gmail.com>
2023-06-11 13:11:47 -0700

    fix animation pause when upgrading meta card

--- scripts/meta_upgrade_card.gd
+++ scripts/meta_upgrade_card.gd
@@ -2,10 +2,12 @@ class_name UIMetaUpgradeCard
 extends PanelContainer
 
 var disabled = false
+var upgrade: MetaUpgrade
 
 @onready var name_label = %NameLabel
 @onready var description_label = %DescriptionLabel
 @onready var purchase_button = %PurchaseButton
+@onready var progress_bar = %ProgressBar
 
 
 func _ready():
@@ -16,10 +18,19 @@ func select_card():
 	$AnimationPlayer.play("selected")
 	disabled = true
 
+	await $AnimationPlayer.animation_finished
+	disabled = false
+
 
 func set_meta_upgrade(upgrade: MetaUpgrade):
+	self.upgrade = upgrade
 	name_label.text = upgrade.name
 	description_label.text = upgrade.description
+
+
+func update_progress(value):
+	var percent = MetaProgression.save_data
+	pass
 
 
 func on_purchase_pressed():
