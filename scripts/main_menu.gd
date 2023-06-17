4ff93edd81826c43d500293772fa18befac75fcf
symphony <jciurc@gmail.com>
2023-06-17 13:39:31 -0700

    add meta menu to game menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -5,6 +5,7 @@ var options_scene = preload("res://scenes/options_menu.tscn")
 
 func _ready():
 	%PlayButton.pressed.connect(on_play_pressed)
+	%UpgradesButton.pressed.connect(on_upgrades_pressed)
 	%OptionsButton.pressed.connect(on_options_pressed)
 	%QuitButton.pressed.connect(on_quit_pressed)
 
@@ -13,6 +14,12 @@ func on_play_pressed():
 	ScreenTransition.transition()
 	await ScreenTransition.transitioned_halfway
 	get_tree().change_scene_to_file("res://scenes/_main_game.tscn")
+
+
+func on_upgrades_pressed():
+	ScreenTransition.transition()
+	await ScreenTransition.transitioned_halfway
+	get_tree().change_scene_to_file("res://scenes/meta_menu.tscn")
 
 
 func on_options_pressed():
