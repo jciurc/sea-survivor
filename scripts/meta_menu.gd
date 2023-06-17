146f887749118ed670f9e6b31ac9d039f27005fe
symphony <jciurc@gmail.com>
2023-06-17 13:41:26 -0700

    add dummy cards in meta menu for testing

--- scripts/meta_menu.gd
+++ scripts/meta_menu.gd
@@ -8,6 +8,9 @@ var meta_upgrade_card_scene = preload("res://scenes/meta_upgrade_card.tscn")
 
 
 func _ready():
+	for child in grid_container.get_children():
+		child.queue_free()
+
 	for upgrade in upgrades:
 		var card_instance = meta_upgrade_card_scene.instantiate()
 		grid_container.add_child(card_instance)
