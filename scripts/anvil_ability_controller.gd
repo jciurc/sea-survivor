9d7ad7c876c959dcdfd283d46ec3b27351082624
symphony <jciurc@gmail.com>
2023-07-09 14:29:12 -0700

    add random spawn position logic to anvil controller

--- scripts/anvil_ability_controller.gd
+++ scripts/anvil_ability_controller.gd
@@ -13,3 +13,9 @@ func on_timer_timeout():
 	var player = get_tree().get_first_node_in_group("player") as Node2D
 	if !player:
 		return
+
+	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
+	var random_position = random_direction * randf_range(0, BASE_RANGE)
+	var additional_offset = random_direction * 20
+	var query_parameters = PhysicsRayQueryParameters2D.create(player.global_position, random_position + additional_offset, 1)
+	var collisions = get_tree().root.world_2d.direct_space_state.intersect_ray(query_parameters)
