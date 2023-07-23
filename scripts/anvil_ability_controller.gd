5c2c280f62913f47649f543e583d72efe006746b
symphony <jciurc@gmail.com>
2023-07-22 17:55:19 -0700

    set up anvil count logic in anvil controller

--- scripts/anvil_ability_controller.gd
+++ scripts/anvil_ability_controller.gd
@@ -2,12 +2,16 @@ extends Node
 
 const BASE_RANGE := 100
 const COLLISION_OFFSET := -20 # allow ability to spawn slightly out of bounds
+const BASE_ANVIL_COUNT := 1
+
+var anvil_count = BASE_ANVIL_COUNT
 
 @export var anvil_ability_scene: PackedScene
 
 
 func _ready():
 	$Timer.timeout.connect(on_timer_timeout)
+	GameEvents.ability_upgrade_added.connect(on_ability_upgrade_added)
 
 
 func on_timer_timeout():
@@ -19,3 +23,8 @@ func on_timer_timeout():
 	var anvil_instance = anvil_ability_scene.instantiate()
 	get_tree().get_first_node_in_group("foreground_layer").add_child(anvil_instance)
 	anvil_instance.position = random_position
+
+
+func on_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary):
+	if upgrade.id == "anvil_count":
+		anvil_count = BASE_ANVIL_COUNT + current_upgrades["anvil_count"]["quantity"]
